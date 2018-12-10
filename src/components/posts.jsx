594dc51415bad6c660d5a73a5b29ac858a3664fb
Alex <alernerm@gmail.com>
2018-12-10 14:25:25 -0500

    redirect component

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  //const { sortBy} = queryString.parse(location.search);
+  const result = queryString.parse(location.search);
+  console.log(result);
+
   return (
     <div>
       <h1>Posts</h1>
